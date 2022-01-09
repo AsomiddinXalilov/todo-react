e190b1868ed60e350db1baca9b6e68527afda7b5
AsomiddinXalilov <asomiddinxalilov6@gmail.com>
2022-01-10 02:59:51 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [todo, setTodo] = useState([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>
+      <h1 className='todo__head'>Todo app: {todo.length}</h1>
+
+      <input 
+       className='todo__input'
+       onKeyPress={(e) =>{
+        if (e.code === 'Enter') {
+          let newTodo = {
+            id: new Date().getTime(),
+            content: e.target.value,
+            isCompleted: false,
+          }
+
+          setTodo([newTodo, ...todo])
+          e.target.value = null
+        }
+      }}
+      placeholder={'typing...'}/>
+
+
+      <ul className='todo__list'>
+        {
+          todo.map(item => {
+            return (
+              <li
+              style={{color: item.isCompleted ? 'red' : 'gold'}}
+                className='todo__item'
+                key={item.id}>
+                  <input
+                   onChange={(e) => {
+                    const todoId = e.target.dataset.id
+                    const findTodo = todo.find(i => i.id === Number(todoId))
+                    findTodo.isCompleted = !findTodo.isCompleted
+                   }}
+                   data-id={item.id}
+                   type={'checkbox'}/>
+                {item.content}
+                <button className='todo__button'
+                onClick={(e) => {
+                  let filteredTodo = todo.filter(i => i.id !== item.id)
+                  alert('Delete !!!',setTodo(filteredTodo, ...todo))
+                }}>delete</button>
+              </li> 
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
